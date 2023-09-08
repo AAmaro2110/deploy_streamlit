edcb74e3eaa7a71d977704f72ce3a921b591ecf8
root <kun010@gmail.com>
2023-09-08 17:56:13 +0000

    release version 1.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,20 @@ from google.oauth2 import service_account
 import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-
 db = firestore.Client(credentials=creds, project="names-project-demo")
 
 dbNames = db.collection("names")
-
 st.header("Nuevo registro")
+
 index = st.text_input("Index")
 name = st.text_input("Name")
 sex = st.selectbox(
-  'Select sex',
-  ('F','M','Other')
-)
+  'Select Sex',
+  ('F', 'M', 'Other'))
+
 submit = st.button("Crear nuevo registro")
+
+# Once the name has submitted, upload it to the database
 if index and name and sex and submit:
   doc_ref = db.collection("names").document(name)
   doc_ref.set({
@@ -26,15 +27,11 @@ if index and name and sex and submit:
     "name": name,
     "sex": sex
   })
-  st.sidebar.write ("registro insertado correctamente")
+  st.sidebar.write("Registro insertado correctamente")
 
-names_ref = list(db.collection(u'names').stream())
-names_dict = list(map(lambda x: x.to_dict(), names_ref))
-names_dataframe = pd.DataFrame(names_dict)
-st.dataframe(names_dataframe)
-
-def loadbyName(name):
-  names_ref = dbNames.where(u'name',u'==',name)
+# ...
+def loadByName(name):
+  names_ref = dbNames.where(u'name', u'==', name)
   currentName = None
   for myname in names_ref.stream():
     currentName = myname
@@ -45,33 +42,40 @@ nameSearch = st.sidebar.text_input("nombre")
 btnFiltrar = st.sidebar.button("Buscar")
 
 if btnFiltrar:
-  doc = loadbyName(nameSearch)
+  doc = loadByName(nameSearch)
   if doc is None:
     st.sidebar.write("Nombre no existe")
   else:
     st.sidebar.write(doc.to_dict())
-
-st.sidebar.markdown("""-----""")
+# ...
+st.sidebar.markdown("""---""")
 btnEliminar = st.sidebar.button("Eliminar")
-
 if btnEliminar:
-  deletename = loadbyName(nameSearch)
+  deletename = loadByName(nameSearch)
   if deletename is None:
-    st.sidebar.write(f"{nameSearch} no existe.")
+    st.sidebar.write(f"{nameSearch} no existe")
   else:
     dbNames.document(deletename.id).delete()
-    st.sidebar.write(f"{nameSearch} eliminado.")
+    st.sidebar.write(f"{nameSearch} eliminado")
+#...
 
 st.sidebar.markdown("""---""")
 newname = st.sidebar.text_input("Actualizar nombre")
 btnActualizar = st.sidebar.button("Actualizar")
-
 if btnActualizar:
-  updatename = loadbyName(nameSearch)
+  updatename = loadByName(nameSearch)
   if updatename is None:
-    st.sidebar.write(f"{nameSearch} no existe.")
+    st.write(f"{nameSearch} no existe")
   else:
     myupdatename = dbNames.document(updatename.id)
-    myupdatename.update({
+    myupdatename.update(
+    {
       "name": newname
-    })
+    }
+    )
+# ...
+
+names_ref = list(db.collection(u'names').stream())
+names_dict = list(map(lambda x: x.to_dict(), names_ref))
+names_dataframe = pd.DataFrame(names_dict)
+st.dataframe(names_dataframe)
